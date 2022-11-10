7cc1e9673d235b96ec948741957ef1c7cdae6fc3
JW20221 <114418889+JW20221@users.noreply.github.com>
2022-11-10 09:29:36 +0100

    Added RoomControllerTest class, to be continued.

---
+++ src/test/java/bookaroom/v1/test/RoomControllerTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookaroom.v1.test;
+
+import bookaroom.v1.controllers.RoomController;
+import bookaroom.v1.controllers.LoginController;
+import bookaroom.v1.exceptions.DoesNotExistException;
+import bookaroom.v1.models.Room;
+import bookaroom.v1.models.User;
+import bookaroom.v1.database.MockDatabase;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author jingminwang
+ */
+public class RoomControllerTest {
+    
+}
